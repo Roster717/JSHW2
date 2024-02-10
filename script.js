4720157d6c9ffe4f67467787657fc494c74b543c
Doniyor <doniyortilavov07@gmail.com>
2024-02-10 13:00:58 +0500

    jshomework

---
+++ script.js
@@ -0,0 +1,26 @@
+var firstName = prompt("Ismingizni kiriting: ")
+console.log("Ismingizni kiriting: " + firstName);
+var lastName = prompt("Yoshingizni kiriting: ")
+console.log("Yoshingizni kiriting: " + lastName)
+
+
+var a = prompt("Misolni yeching: 3+7 = ?")
+console.log("Misol 1: " + "3+7 = " + (3+7) + " Sizning javob: " + a)
+
+var b = prompt("Misolni yeching: 35+5 = ?")
+console.log("Misol 2: " + "35+5 = " + (35+5) + " Sizning javob: " + b)
+
+var c = prompt("Misolni yeching: 110-10 = ?")
+console.log("Misol 3: " + "110-10 = " + (110-10) + " Sizning javob: " + c)
+
+var d = prompt("Misolni yeching: 4*5 = ?")
+console.log("Misol 4: " + "4*5 = " + (4*5) + " Sizning javob: " + d)
+
+var e = prompt("Misolni yeching: 100/100 = ?")
+console.log("Misol 5: " + "100/100 = " + (100/100) + " Sizning javob: " + e)
+
+
+
+
+
+
